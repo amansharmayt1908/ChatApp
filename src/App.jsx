1fe335ed6b3c6413de62f7af074fadc55e36dd0a
Aman Sharma <166890222+amansharmayt1908@users.noreply.github.com>
2025-02-01 23:59:28 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,11 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import RegisterPage from "./components/registerPage";
-import LoginPage from "./components/loginPage";
-import "./App.css";
-import MainPage from "./components/mainPage";
-import ProtectedRoute from './components/ProtectedRoute';
-import ChatPage from './components/chatPage';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<RegisterPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route 
-            path="/main" 
-            element={
-              <ProtectedRoute>
-                <MainPage />
-              </ProtectedRoute>
-            } 
-          />
-          <Route 
-            path="/chat/:chatId" 
-            element={
-              <ProtectedRoute>
-                <ChatPage />
-              </ProtectedRoute>
-            } 
-          />
-          <Route path="/" element={<Navigate to="/login" replace />} />
-          
-        </Routes>
-      </div>
-    </Router>
-  );
+    <div>
+      
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom'
-import App from './App'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import RegisterPage from './components/RegisterPage'
+import LoginPage from './components/LoginPage'
+import MainPage from './components/MainPage'
+import ChatPage from './components/ChatPage'
+import ProtectedRoute from './components/ProtectedRoute'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route 
+            path="/main" 
+            element={
+              <ProtectedRoute>
+                <MainPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route 
+            path="/chat/:chatId" 
+            element={
+              <ProtectedRoute>
+                <ChatPage />
+              </ProtectedRoute>
+            } 
+          />
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          
+        </Routes>
+      </div>
+    </Router>
+    
   </StrictMode>,
 )
